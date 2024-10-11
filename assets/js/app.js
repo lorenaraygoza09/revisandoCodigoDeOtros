162ac21dc0cd4471ddcf7e54fb97e203d2e13eac
Lorena Raygoza <lorenaraygoza0904@gmail.com>
2024-10-11 16:03:26 -0700

    feat: añado archivos html, js y css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,26 @@
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//declare la funcion como async 
+ async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //cambie las comillas simples por `backticks` en las interpolaciones
+  $n.textContent = `${data.name}`; 
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  // añadi el signo $ en n.textContent
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
